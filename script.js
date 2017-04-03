004f334580cf2e3900245f783d598a4b37858861
Shruti <Shruti>
2017-04-03 00:08:25 -0400

    added some more functionality

--- script.js
+++ script.js
@@ -1,99 +1,54 @@
+   function flight (fNum, fDept, fArrival, fDate, fPassengers, fAirline) { 
+            this.fNum = fNum;
+            this.fDept = fDept;
+            this.fArrival = fArrival;
+            this.fDate = fDate;
+            this.fPassengers = [];
+            this.fAirline = fAirline;
+          }
 
-function flight (fNum, fDept, fArrival, fDate, fPassengers) { 
-	this.fNum = fNum;
-	this.fDept = fDept;
-	this.fArrival = fArrival;
-	this.fDate = fDate;
-	this.fPassengers = [];
-}
+      //socialize - do they like to talk, sleep - do they like to sleep, kids - do they have kids, mobility - do they get up often, occasion - conference, concert etc
+          function passenger (fname, lname, socialize, sleep, kids, mobility, notes, occasion, seatNumber) {
+            this.fname = fname; 
+            this.lname = lname;
+            this.sleep = sleep;
+            this.socialize = socialize;
+            this.kids = kids;
+            this.mobility = mobility;
+            this.notes = notes;
+            this.occasion = occasion;
+            this.seatNumber = seatNumber;
 
-flightCHNY = new flight(234, "Chicago", "New York", "2017-01-01");
-flightNYCH = new flight(345, "New York", "Chicago", "2017-02-02");
-flightSFNY = new flight(456, "San Francisco", "New York", "2017-03-03");
-flightNYSF = new flight(567, "New York", "San Francisco", "2017-04-04");
-flightSFCH = new flight(678, "San Francisco", "Chicago", "2017-05-05");
-flightCHSF = new flight(789, "Chicago", "San Francisco", "2017-06-06");
+          }
 
-var array = []
-array.push(flightCHNY);
-array.push(flightNYCH);
-array.push(flightSFNY);
-array.push(flightNYSF);
-array.push(flightSFCH);
-array.push(flightCHSF);
+          flightCHNY = new flight(234, "Chicago", "New York", "2017-01-01", "Delta");
+          flightNYCH = new flight(345, "New York", "Chicago", "2017-02-02", "Southwest");
+          flightSFNY = new flight(456, "San Francisco", "New York", "2017-03-03", "JetBlue");
+          flightNYSF = new flight(567, "New York", "San Francisco", "2017-04-04", "American");
+          flightSFCH = new flight(678, "San Francisco", "Chicago", "2017-05-05", "Delta");
+          flightCHSF = new flight(789, "Chicago", "San Francisco", "2017-06-06", "American");
 
-//adding passengers to flight
-flightCHNY.fPassengers.push("Emily Shu");
-flightCHNY.fPassengers.push("Divya Bhaskara");
+          var array = []
+          array.push(flightCHNY);
+          array.push(flightNYCH);
+          array.push(flightSFNY);
+          array.push(flightNYSF);
+          array.push(flightSFCH);
+          array.push(flightCHSF);
 
+          //adding passengers to flight
+          pass1 = new passenger ("Emily", "Shu", "yes", "no", "no", "sometimes", "I might nap occasionally but otherwise I love to talk", "Forbes women's entreprenuership conference", "A23")
+          flightCHNY.fPassengers.push(pass1);
+          pass2 = new passenger ("Divya", "Bhaskara", "no", "yes", "no", "sometimes", "I am coming back from a conference so I will be very tired", "none", "B32") 
+          flightCHNY.fPassengers.push(pass2);
+          pass3 =  ("Himica", "Kumar", "yes", "no", "no", "rarely", "I would love to get to know people that are headed to the same destination", "Adele concert", "C40")
+          flightNYSF.fPassengers.push(pass3);
+          pass4 =  ("Shruti", "Mhasawade", "yes", "no", "no", "rarely", "I would love to get to know people that are headed to the same destination", "Adele concert", "C40")
+          //Going to add an array of passengers on a specific flight to keep it simple for now
+          /*function passenger (pName, pFlight) { 
+            this.pName = pName; 
+          }*/
 
-//Going to add an array of passengers on a specific flight to keep it simple for now
-function passenger (pName, pFlight) { 
- 	this.pName = pName; 
-}
-
-function processFNumberForm() {
-    var flightNumber = document.searchByNumber.flightNumber.value;
-    var deptDate = document.searchByNumber.deptDate.value;
-   
-    console.log("flight dept date: "  + deptDate + "fnum " + flightNumber);
-
-    //console.log(document.getElementById("flightInfo").innerHTML);
-
-    //search through array of flight objects to see if flight num matches any
-
-    for (var i = 0; i < array.length; i++) { 
-    	console.log(array[i].fNum); //gets the flight numbers of all stored flights
-    	if (flightNumber == array[i].fNum) { 
-    		//console.log("FLIGHT FOUND!!");	
-    		//document.write("FLIGHT FOUND");
-    		//DO SOMETHING HERE!
-    		displayFlightDetails(array[i]);
-    	}
-    }   
-}
-
-
-function processLocationForm() { 
-
-	    var departure = document.searchByLocation.departure.value;
-	    var arrival = document.searchByLocation.arrival.value;
-	    var date = document.searchByLocation.deptDate.value;
-
-	    console.log("Dept: " + departure + "Arrival: " + arrival + "Date: " + date);
-
-	    for (var i = 0; i < array.length; i++) { 
-    	//console.log(array[i].fNum); //gets the flight numbers of all stored flights
-    	if (departure == array[i].fDept && arrival == array[i].fArrival && date == array[i].fDate) { 
-    		//console.log("FLIGHT FOUND!!");	
-    		displayFlightDetails(array[i]);
-    		//DO SOMETHING HERE!
-    	}
-    }
-}
-
-function displayFlightDetails(flight) { 
-	document.write("Flight number " + flight.fNum + " leaving from " + flight.fDept + " and arriving at  " + flight.fArrival + " on " + flight.fDate);
-	document.write("<br>");
-	console.log(flight.fPassengers);
-	if (flight.fPassengers.length == 0) { 
-		document.write("There are no passengers registered on AirBae for this flight yet. Be the first!");
-		document.write("<br>");
-
-	} else { 
-		document.write("Current Passengers on this flight include: " + flight.fPassengers + " ");
-		document.write("<br>");
-
-
-	} 
-
-
-
-
-
-}
-
-
-
-
-
+          var tempFlightNumber = "";
+          var tempDate = "";
+          var setTemp = false;
